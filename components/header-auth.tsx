dc14a973ef1e64a92ec72614309c32a537cf0fef
Silas Rhyneer <49383285+CaptainCrouton89@users.noreply.github.com>
2025-03-22 18:02:33 -0700

    Initial commit

---
+++ components/header-auth.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { signOutAction } from "@/app/actions";
+import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import { createClient } from "@/utils/supabase/client";
+import { User } from "@supabase/supabase-js";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { ThemeToggleWrapper } from "./theme-toggle-wrapper";
+import { Button } from "./ui/button";
+
+export default function AuthButton() {
+  const supabase = createClient();
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const { data } = await supabase.auth.getUser();
+      setUser(data.user);
+    };
+
+    fetchUser();
+  }, [supabase]);
+
+  if (!hasEnvVars) {
+    return (
+      <>
+        <div className="flex gap-4 items-center">
+          <div className="flex gap-2 items-center">
+            <ThemeToggleWrapper />
+            <Button
+              asChild
+              size="sm"
+              variant={"outline"}
+              disabled
+              className="opacity-75 cursor-none pointer-events-none"
+            >
+              <Link href="/sign-in">Sign in</Link>
+            </Button>
+            <Button
+              asChild
+              size="sm"
+              variant={"default"}
+              disabled
+              className="opacity-75 cursor-none pointer-events-none"
+            >
+              <Link href="/sign-up">Sign up</Link>
+            </Button>
+          </div>
+        </div>
+      </>
+    );
+  }
+  return user ? (
+    <div className="flex items-center gap-4">
+      Hey, {user.email}!
+      <ThemeToggleWrapper />
+      <form action={signOutAction}>
+        <Button type="submit" variant={"outline"}>
+          Sign out
+        </Button>
+      </form>
+    </div>
+  ) : (
+    <div className="flex gap-2 items-center">
+      <ThemeToggleWrapper />
+      <Button asChild size="sm" variant={"outline"}>
+        <Link href="/sign-in">Sign in</Link>
+      </Button>
+      <Button asChild size="sm" variant={"default"}>
+        <Link href="/sign-up">Sign up</Link>
+      </Button>
+    </div>
+  );
+}
